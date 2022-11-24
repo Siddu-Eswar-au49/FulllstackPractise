9843e0d8ad3a180b2c7178f2b3189dbd4007168a
Siddu Eswar <siddueshu@gmail.com>
2022-11-24 12:39:34 +0530

    dommodification

--- javascript/static/domManipulation/manipulate.js
+++ javascript/static/domManipulation/manipulate.js
@@ -1,6 +1,23 @@
-const alldivs = document.querySelectorAll("div");
-
+const alldivs = [];
+const bodyEle = document.querySelector("main");
 let currentPlayer = "o";
+
+for (let i = 0; i < 18; i++) {
+  const divEle = document.createElement("div");
+  divEle.id = i.toString();
+  divEle.className = "tile";
+  divEle.dataset.player = "none";
+  divEle.dataset.currentplayer = currentPlayer;
+
+  const spanEle = document.createElement("span");
+
+  divEle.append(spanEle);
+  bodyEle.append(divEle);
+  alldivs.push(divEle);
+}
+
+// all divs are present in html
+// alldivs = document.querySelectorAll("div");
 
 alldivs.forEach((div) => {
   div.addEventListener("click", (e) => {
@@ -17,6 +34,7 @@ alldivs.forEach((div) => {
         // curre = x
         currentPlayer = "o";
       }
+      //check win status
       changeAllcurrentplayers();
     }
   });
